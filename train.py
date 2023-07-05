e8d80998071c922631f94a35165f2326c4e2347f
JeremyWYQ <122764915+JeremyWYQ@users.noreply.github.com>
2023-07-05 20:24:10 +0400

    Add files via upload

---
+++ train.py
@@ -0,0 +1,103 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+# Set random seed for reproducibility
+random_seed = 1
+torch.backends.cudnn.enabled = False
+torch.manual_seed(random_seed)
+
+# Hyperparameters
+n_epochs = 3
+batch_size_train = 64
+batch_size_test = 1000
+learning_rate = 0.01
+momentum = 0.5
+log_interval = 10
+
+# Setting up the environment
+model = torchvision.models.resnet18(pretrained=False)
+model.fc = nn.Linear(512, 10)
+
+train_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('./data/', train=True, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size_train, shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(
+    torchvision.datasets.MNIST('./data/', train=False, download=True,
+                               transform=torchvision.transforms.Compose([
+                                   torchvision.transforms.ToTensor(),
+                                   torchvision.transforms.Normalize(
+                                       (0.1307,), (0.3081,))
+                               ])),
+    batch_size=batch_size_test, shuffle=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i * len(train_loader.dataset) for i in range(n_epochs + 1)]
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(F.log_softmax(output, dim=1), target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+            train_losses.append(loss.item())
+            train_counter.append(
+                (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset)))
+            torch.save(model.state_dict(), './results/model.pth')
+            torch.save(optimizer.state_dict(), './results/optimizer.pth')
+
+# def test():
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data, target in test_loader:
+#             data, target = data.to(device), target.to(device)
+#             output = model(data)
+#             test_loss += F.nll_loss(F.log_softmax(output, dim=1), target, reduction='sum').item()
+#             pred = output.argmax(dim=1, keepdim=True)
+#             correct += pred.eq(target.view_as(pred)).sum().item()
+#     test_loss /= len(test_loader.dataset)
+#     test_losses.append(test_loss)
+#     print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+#         test_loss, correct, len(test_loader.dataset),
+#         100. * correct / len(test_loader.dataset)))
+
+# model.load_state_dict(torch.load('./results/model.pth'))
+# optimizer.load_state_dict(torch.load('./results/optimizer.pth'))
+
+# test()
+# for epoch in range(1, n_epochs + 1):
+#     train(epoch)
+#     test()
+
+# plt.plot(train_counter, train_losses, color='blue')
+# plt.scatter(test_counter, test_losses, color='red')
+# plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+# plt.xlabel('number of training examples seen')
+# plt.ylabel('negative log likelihood loss')
+# plt.show()
+
